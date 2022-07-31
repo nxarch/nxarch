c1f21b02abff34a8f905693daec6ed059359179b
Boetzkes, Yannick <code@yannickboetzkes.com>
2022-07-31 14:16:56 +0200

    fix(ng-nest): remove console log

--- packages/ng-nest/src/generators/init/runners/update-package-json.ts
+++ packages/ng-nest/src/generators/init/runners/update-package-json.ts
@@ -3,7 +3,6 @@ import { InitGeneratorSchema } from '../schema';
 
 export async function updatePackageJson(tree: Tree, options: InitGeneratorSchema) {
   updateJson(tree, 'package.json', (pkgJson) => {
-    console.log(pkgJson);
     pkgJson.scripts = pkgJson.scripts ?? {};
     pkgJson.scripts['dev:server'] = `nx ${options.serverApp}:serve-ssr:development`;
 
