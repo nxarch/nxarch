a5e2cb613f78c93e427ebb8ab30f595a52ea17a4
Boetzkes, Yannick <code@yannickboetzkes.com>
2022-08-26 09:14:17 +0200

    feat(ng-nest): add nguniversal dependency

--- packages/ng-nest/src/generators/init/runners/add-dependencies.ts
+++ packages/ng-nest/src/generators/init/runners/add-dependencies.ts
@@ -8,6 +8,7 @@ export function addDependencies(tree: Tree) {
       '@nestjs/common': '^9.0.0',
       '@nestjs/core': '^9.0.0',
       '@nestjs/platform-express': '^9.0.0',
+      '@nguniversal/express-engine': '^14.1.0',
       '@nxarch/nest-nguniversal': '^0.3.0',
       'reflect-metadata': '^0.1.13',
       'class-transformer': '^0.4.0',
